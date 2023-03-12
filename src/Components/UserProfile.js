6ff970e044721883be4e320fb2a2e0d43f88e308
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-03-12 16:27:44 +0530

    First commit

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -73,15 +73,15 @@ const UserProfile = () => {
             />
             <img
               className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover"
-              src={user.image}
+              src={user?.image}
               alt="user-pic"
             />
           </div>
           <h1 className="font-bold text-3xl text-center mt-3">
-            {user.userName}
+            {user?.userName}
           </h1>
           <div className="absolute top-0 z-1 right-0 p-2">
-            {userId === User._id && (
+            {userId === user?._id && (
               <div
               // clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
               >
